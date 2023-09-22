604e75a73ea5fad9fd26d34c5a9027b02cac0352
khushbu <khushbusoni475@gmail.com>
2023-09-22 23:54:25 +0530

    [khushbu] Adding some logical programs

---
+++ Remap_LogicalProgram/LogicalPrograms.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Remap_LogicalProgram
+{
+    internal class LogicalPrograms
+    {
+        public void FriendsName(string a, string b, string c, string d, string e)
+        {
+            Console.WriteLine("friends name : {0} {1} {2} {3} {4}", a, b, c, d, e);
+
+        }
+        public void SquareOfTwoNumbers(float x, float y)
+        {
+            //int sum = 0;
+            float squareOfSum = 0;
+            float squareOfDifference = 0;
+            squareOfSum = ((x + y) * (x + y));
+            Console.WriteLine("Square of sum of two numbers is : {0}", squareOfSum);
+            squareOfDifference = ((x - y) * (x - y));
+            Console.WriteLine("Square of difference of two numbers is : {0}", squareOfDifference);
+        }
+        public void UpperCaseLowerCase(string placeToVisit)
+        {
+
+        }
+        public void TenTimesPrintString(string num)
+        {
+            if (int.TryParse(num, out int number))
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    string result = num.ToString();
+                    Console.WriteLine(i + " times the number is: {0}", result);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
+        }
+        public static void PersonFullName()
+        {
+            Console.WriteLine("Enter a person's full name:");
+            string fullName = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(fullName))
+            {
+                Console.WriteLine("Please enter a valid name.");
+
+            }
+            string[] parts = fullName.Split(' ');
+            int partsCount = parts.Length;
+
+            Console.WriteLine("Number of parts in the name: " + partsCount);
+        }
+        public static void RandomNumbers()
+        {
+
+            Random random = new Random();
+            int[] numbers = new int[5];
+            double sum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(10, 51);
+                Console.Write(numbers[i] + " ");
+                sum += numbers[i];
+            }
+
+            double average = sum / numbers.Length;
+            Console.WriteLine("\nAvarage of 5 random numbers is : {0}", average);
+        }
+        public static void AreaAndPerimeterOfCircle(double diameter)
+        {
+
+            double radius = diameter / 2;
+            double perimeter = 2 * Math.PI * radius;
+            double area = Math.PI * Math.Pow(radius, 2);
+
+            Console.WriteLine("Perimeter of the circle: {0} ", perimeter);
+            Console.WriteLine("Area of the circle: {0} ", area);
+        }
+
+        public static void  GetNextDay(int y, int m, int d)
+        {
+            if(m <1 || m>12)
+            {
+                Console.WriteLine("Invalid month");
+                return;
+            }
+            if(m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12)
+            {
+                if (d < 1 || d > 31)
+                {
+                    Console.WriteLine("Invalid date");
+                    return;
+                }
+
+                d = d + 1;
+                if(d > 31)
+                {
+                    d = 1; m = m + 1;
+                    if(m>12)
+                    {
+                        m = 1; y = y + 1;
+                        
+                    }
+                    
+                }
+                Console.WriteLine("Next day is : {0} {1} {2}", d, m, y);
+                return;
+            }
+            if (m == 4 || m == 6 || m == 9 || m == 11)
+            {
+                if (d < 1 || d > 30)
+                {
+                    Console.WriteLine("Invalid date");
+                    return;
+                }
+                d = d + 1;
+                if (d > 30)
+                {
+                    d = 1; m = m + 1;
+                    if (m > 12)
+                    {
+                        m = 1; y = y + 1;
+
+                    }
+                }
+                Console.WriteLine("Next day is : {0} {1} {2}", d, m, y);
+                return;
+            }
+            if(m == 2)
+            {
+                if ((y % 4 == 0 && y % 100 != 0) || (y % 400 == 0))
+                {
+                    if(d < 1 || d > 29)
+                    {
+
+                        Console.WriteLine("Invalid date");
+                        return;
+                    }
+                    d = d + 1;
+                    if (d > 29)
+                    {
+                        d = 1; m = m + 1;
+                        if (m > 12)
+                        {
+                            m = 1; y = y + 1;
+
+                        }
+                    }
+                    Console.WriteLine("Next day is : {0} {1} {2}", d, m, y);
+                    return;
+                }
+
+                else if(d < 1 || d > 28)
+                {
+                    Console.WriteLine("Invalid date");
+                    return;
+                }
+                d = d + 1;
+                if (d > 28)
+                {
+                    d = 1; m = m + 1;
+                    if (m > 12)
+                    {
+                        m = 1; y = y + 1;
+
+                    }
+                }
+                Console.WriteLine("Next day is : {0} {1} {2}", d, m, y);
+                return;
+            }
+        }
+        public static void TimeElapseCalulation()
+        {
+            Console.WriteLine("please enter any key to start stopwatch");
+            Console.ReadLine();
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            Console.WriteLine("stopwatch start");
+
+            Console.WriteLine("\nplease enter any key to print series");
+            Console.ReadLine();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("please enter any key to stop stopwatch");
+            Console.ReadLine();
+
+            stopwatch.Stop();
+
+            TimeSpan Elapsed = stopwatch.Elapsed;
+            Console.WriteLine("stopwatch stoped");
+
+            Console.WriteLine("Time elapded: {0}", Elapsed.TotalMilliseconds);
+            Console.ReadLine();
+        }
+        public static void NumberDivisibleBySeven()
+        {
+            string result = " ";
+            for (int i = 2000;  i <=3200 ; i++)
+            {
+                
+                if(i % 7 == 0 && i % 5 != 0)
+                {
+                    result += i + ",";
+                   
+                }
+
+            }
+            Console.WriteLine("num is  divisible by 7 are : {0}", result);
+        }
+        public static void FactorialNumber(int num)
+        {
+            int factorial = 1;
+            
+            for(int i = 1; i <= num; i++)
+            {
+                factorial = factorial * i;
+            }
+            Console.WriteLine("Factorial of number {0}: is {1}",num, factorial);
+        }
+
+        static int n = 1;
+        public static void PrintSeriesWithoutUsingLoop()
+        {
+            Console.WriteLine(n);
+            n++;
+
+            if(n<=100)
+            {
+                PrintSeriesWithoutUsingLoop();
+               // Console.WriteLine(n);
+            }
+           
+        }
+        public static void RootOfQuadraticEquation()
+        {
+            double a, b, c;
+            double discriminant, root1, root2, realPart, imagPart;
+
+            Console.WriteLine("Enter coefficients a, b and c:");
+            a = Convert.ToDouble(Console.ReadLine());
+            b = Convert.ToDouble(Console.ReadLine());
+            c = Convert.ToDouble(Console.ReadLine());
+
+            discriminant = b * b - 4 * a * c;
+
+            if (discriminant > 0)
+            {
+                root1 = (-b + Math.Sqrt(discriminant)) / (2 * a);
+                root2 = (-b - Math.Sqrt(discriminant)) / (2 * a);
+                Console.WriteLine("root1 = {0} and root2 = {1}", root1, root2);
+            }
+            else if (discriminant == 0)
+            {
+                root1 = root2 = -b / (2 * a);
+                Console.WriteLine("root1 = root2 = {0};", root1);
+            }
+            else
+            {
+                realPart = -b / (2 * a);
+                imagPart = Math.Sqrt(-discriminant) / (2 * a);
+                Console.WriteLine("root1 = {0}+{1}i and root2 = {0}-{1}i", realPart, imagPart);
+            }
+        }
+
+        public static void NumbersFactorOutputInString(int num)
+        {
+            if (num % 7 == 0 )
+                Console.Write("Plong");
+
+            if (num % 5 == 0 )
+                Console.Write("Plang");
+
+            if (num % 3 == 0 )
+                Console.Write("Pling");
+
+            //else if(num % 3 == 0 && num % 5 == 0)
+            //Console.WriteLine("The number has 3 and 5 as the factor so the output is : PlingPlang");
+
+            //else if (num % 3 == 0 && num % 7 == 0)
+            //    Console.WriteLine("The number has 3 and 7 as the factor so the output is : PlingPlong");
+
+            //else if(num % 5 == 0 && num % 7 == 0)
+            //    Console.WriteLine("The number has 5 and 7 as the factor so the output is : PlangPlong");
+
+            //else if (num % 3 == 0 && num % 5 == 0 && num % 7 == 0)
+            //    Console.WriteLine("The number has 5 and 7 as the factor so the output is : PlangPlongPling");
+
+            if(num % 7 != 0 && num % 5 != 0 && num % 3 != 0)
+                Console.WriteLine(num);
+
+
+        }
+        public static void LetterPresenrOrAbsent()
+        {
+            Console.WriteLine("Enter any string");
+            string input = Console.ReadLine();
+
+            bool isPresent = (input.Contains("a") && input.Contains("e") && input.Contains("p"));
+            bool isAnyOnePresent = (input.Contains("a") || input.Contains("e") || input.Contains("p"));
+
+            if (isPresent)
+                Console.WriteLine("All present");
+
+            else if (isAnyOnePresent)
+                Console.WriteLine("One or more present");
+
+            else
+                Console.WriteLine("None present");
+        }
+    }
+}
+    
+
---
+++ Remap_LogicalProgram/Program.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Remap_LogicalProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool execute = true;
+            while (execute)
+
+            {
+                Console.WriteLine("Choose any program");
+            Console.WriteLine("1. Name Of your friends\n" +
+                "2.Sqaure of two numbers\n" +
+                "3.Uppercase & Lowercase\n" +
+                "4.10 Times Print String\n" +
+                "5.Persons full name\n" +
+                "6.Random numbers\n" +
+                "7.Area & perimeter of circle\n" +
+                "8. Date, month, year\n" +
+                "9.Time elapse calculation\n" +
+                "10.Number divisible by 7\n" +
+                "11. Factorial number\n" +
+                "12. Print 1 to 100 Without Using Loop\n" +
+                "13. Root of quadratic equation\n" +
+                "14. Number's factor on string output\n" +
+                "15. Letter Presenr Or Absent");
+            int option = Convert.ToInt32(Console.ReadLine());
+            
+            
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Enter 1st Name");
+                        string a = Console.ReadLine();
+                        Console.WriteLine("Enter 2nd Name");
+                        string b = Console.ReadLine();
+                        Console.WriteLine("Enter 3rd Name");
+                        string c = Console.ReadLine();
+                        Console.WriteLine("Enter 4th Name");
+                        string d = Console.ReadLine();
+                        Console.WriteLine("Enter 5th Name");
+                        string e = Console.ReadLine();
+                        LogicalPrograms logicalPrograms = new LogicalPrograms();
+                        logicalPrograms.FriendsName(a, b, c, d, e);
+                        Console.WriteLine("\nWould you like to continue(1) or exit the program(0)");
+                        int desicion = Convert.ToInt32(Console.ReadLine());
+                        if(desicion == 0)
+                        {
+                            execute = false;
+                        }
+
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter 1st Number");
+                        float x = float.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter 2nd Number");
+                        float y = float.Parse(Console.ReadLine());
+                        LogicalPrograms logicalPrograms1 = new LogicalPrograms();
+                        logicalPrograms1.SquareOfTwoNumbers(x, y);
+                        Console.WriteLine("\nWould you like to continue(1) or exit the program(0)");
+                        int desicion1 = Convert.ToInt32(Console.ReadLine());
+                        if (desicion1 == 0)
+                        {
+                            execute = false;
+                        }
+
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter name to visit");
+                        string placeToVisit = Console.ReadLine();
+                        string lowerCasePlace = placeToVisit.ToLower();
+                        string upperCasePlace = placeToVisit.ToUpper();
+                        LogicalPrograms logicalPrograms2 = new LogicalPrograms();
+                        logicalPrograms2.UpperCaseLowerCase(placeToVisit);
+                        Console.WriteLine("place to visit in lower case : {0}", lowerCasePlace);
+                        Console.WriteLine("place to visit in upper case : {0}", upperCasePlace);
+                        Console.WriteLine("\nWould you like to continue(1) or exit the program(0)");
+                        int desicion2 = Convert.ToInt32(Console.ReadLine());
+                        if (desicion2 == 0)
+                        {
+                            execute = false;
+                        }
+
+                        break;
+                    case 4:
+                        Console.WriteLine("Enter a number:");
+                        string num = Console.ReadLine();
+                        LogicalPrograms logicalPrograms3 = new LogicalPrograms();
+                        logicalPrograms3.TenTimesPrintString(num);
+                        Console.WriteLine("\nWould you like to continue(1) or exit the program(0)");
+                        int desicion3 = Convert.ToInt32(Console.ReadLine());
+                        if (desicion3 == 0)
+                        {
+                            execute = false;
+                        }
+
+                        break;
+                    case 5:
+                        LogicalPrograms.PersonFullName();
+                        Console.WriteLine("\nWould you like to continue(1) or exit the program(0)");
+                        int desicion4 = Convert.ToInt32(Console.ReadLine());
+                        if (desicion4 == 0)
+                        {
+                            execute = false;
+                        }
+
+                        break;
+                    case 6:
+                        LogicalPrograms.RandomNumbers();
+                        Console.WriteLine("\nWould you like to continue(1) or exit the program(0)");
+                        int desicion5 = Convert.ToInt32(Console.ReadLine());
+                        if (desicion5 == 0)
+                        {
+                            execute = false;
+                        }
+                        break;
+                    case 7:
+                        Console.Write("Enter Diameter: ");
+                        double diameter = Convert.ToDouble(Console.ReadLine());
+                        LogicalPrograms.AreaAndPerimeterOfCircle(diameter);
+                        Console.WriteLine("\nWould you like to continue(1) or exit the program(0)");
+                        int desicion6 = Convert.ToInt32(Console.ReadLine());
+                        if (desicion6 == 0)
+                        {
+                            execute = false;
+                        }
+                        break;
+                    case 8:
+                        Console.Write("Input a year: ");
+                        int year = Convert.ToInt32(Console.ReadLine());
+
+                        Console.Write("Input a month [1-12]: ");
+                        int month = Convert.ToInt32(Console.ReadLine());
+
+                        Console.Write("Input a day [1-31]: ");
+                        int day = Convert.ToInt32(Console.ReadLine());
+
+                        LogicalPrograms.GetNextDay(year, month, day);
+                        Console.WriteLine("\nWould you like to continue(1) or exit the program(0)");
+                        int desicion7 = Convert.ToInt32(Console.ReadLine());
+                        if (desicion7 == 0)
+                        {
+                            execute = false;
+                        }
+                        //DateTime nextDay = inputDate.AddDays(1);
+
+                        //Console.WriteLine("The next date is [yyyy-MM-dd] " + nextDay.ToString("yyyy-MM-dd"));
+
+                        break;
+                    case 9:
+                        LogicalPrograms.TimeElapseCalulation();
+                        Console.WriteLine("\nWould you like to continue(1) or exit the program(0)");
+                        int desicion8 = Convert.ToInt32(Console.ReadLine());
+                        if (desicion8 == 0)
+                        {
+                            execute = false;
+                        }
+                        break;
+                    case 10:
+                        LogicalPrograms.NumberDivisibleBySeven();
+                        Console.WriteLine("\nWould you like to continue(1) or exit the program(0)");
+                        int desicion9 = Convert.ToInt32(Console.ReadLine());
+                        if (desicion9 == 0)
+                        {
+                            execute = false;
+                        }
+                        break;
+                    case 11:
+                        Console.WriteLine("Enter any number of which factorial you want");
+                        int number = Convert.ToInt32(Console.ReadLine());
+                        LogicalPrograms.FactorialNumber(number);
+                        Console.WriteLine("\nWould you like to continue(1) or exit the program(0)");
+                        int desicion10 = Convert.ToInt32(Console.ReadLine());
+                        if (desicion10 == 0)
+                        {
+                            execute = false;
+                        }
+                        break;
+                    case 12:
+                        LogicalPrograms.PrintSeriesWithoutUsingLoop();
+                        Console.WriteLine("\nWould you like to continue(1) or exit the program(0)");
+                        int desicion11 = Convert.ToInt32(Console.ReadLine());
+                        if (desicion11 == 0)
+                        {
+                            execute = false;
+                        }
+                        break;
+                    case 13:
+                        LogicalPrograms.RootOfQuadraticEquation();
+                        Console.WriteLine("\nWould you like to continue(1) or exit the program(0)");
+                        int desicion12 = Convert.ToInt32(Console.ReadLine());
+                        if (desicion12 == 0)
+                        {
+                            execute = false;
+                        }
+                        break;
+                    case 14:
+                        Console.WriteLine("Enter a number:");
+                        int digit = Convert.ToInt32(Console.ReadLine());
+                        LogicalPrograms.NumbersFactorOutputInString(digit);
+                        Console.WriteLine("\nWould you like to continue(1) or exit the program(0)");
+                        int desicion13 = Convert.ToInt32(Console.ReadLine());
+                        if (desicion13 == 0)
+                        {
+                            execute = false;
+                        }
+                        break;
+                    case 15:
+                        LogicalPrograms.LetterPresenrOrAbsent();
+                        Console.WriteLine("\nWould you like to continue(1) or exit the program(0)");
+                        int desicion14 = Convert.ToInt32(Console.ReadLine());
+                        if (desicion14 == 0)
+                        {
+                            execute = false;
+                        }
+                        break;
+                }
+            }
+            
+            
+        }
+    }
+}
